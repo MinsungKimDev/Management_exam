b66c483314f7a8c1d75b739a23284aa483128c63
MinsungKimDev <minsungkim.dev@gmail.com>
2022-05-04 13:38:17 +0900

    structuring components

--- src/App.js
+++ src/App.js
@@ -3,21 +3,50 @@ import Customer from './components/Customer';
 import './App.css';
 
 
-const customer = {
+const customers = [
+{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
   'name': '김민성',
   'birthday': '990126',
   'gender': '남자',
   'job': '대학생'
-}
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday': '960305',
+  'gender': '남자',
+  'job': '프로그래머'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '아무개',
+  'birthday': '970204',
+  'gender': '남자',
+  'job': '디자이너'
+},
+]
 class App extends Component{
   render(){
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customers.map(c => {
+            return(
+            <Customer
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />);
+          })
+        }
+      </div>
     );
   }
 }
