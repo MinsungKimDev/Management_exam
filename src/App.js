3bcdbf6fbcd89afd01025151bdee00bd9a6ede0a
MinsungKimDev <minsungkim.dev@gmail.com>
2022-05-04 13:06:02 +0900

    using class in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+import { Component } from 'react';
+class App extends Component{
+  render(){
+    return (
+      <div className="gray-background">
+        <img src={logo} lat="logo"></img>
+        <h2>Let's develop management system!</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
