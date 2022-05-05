5d5008991461c557db49de42c5bdfc0bda1d87be
MinsungKimDev <minsungkim.dev@gmail.com>
2022-05-05 21:48:09 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
-import { Paper } from '@material-ui/core';
-import { Table } from '@material-ui/core';
-import { TableHead } from '@material-ui/core';
-import { TableBody } from '@material-ui/core';
-import { TableRow } from '@material-ui/core';
-import { TableCell } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme =>({
@@ -17,25 +18,35 @@ const styles = theme =>({
   },
   table:{
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
 
 class App extends Component{
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
-      .then(res => this.setState({customers: res}))
-      .catch(err => console.log(err));
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
   }
 
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({ completed: completed > 100 ? 0 : completed + 1});
   }
 
   render(){
@@ -56,7 +67,13 @@ class App extends Component{
           <TableBody>
             { this.state.customers ? this.state.customers.map(c => { 
                 return (<Customer id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />) 
-              }) : "" }
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
           </TableBody>
         </Table>
       </Paper>
