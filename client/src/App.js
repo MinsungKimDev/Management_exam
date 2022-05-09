fec24fb37619e610bbd840eec8cf063dadd92dd9
MinsungKimDev <minsungkim.dev@gmail.com>
2022-05-09 16:30:32 +0900

    Add Search Function by Add Filter

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
 import Customer from './components/Customer';
 import CustomerAdd from './components/CustomerAdd';
 import './App.css';
@@ -104,15 +104,19 @@ class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      customers: '',
-      completed: 0
+      customers: [],
+      completed: 0,
+      searchKeyword: ''
     }
+    this.stateRefresh = this.stateRefresh.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
   }
 
   stateRefresh = () => {
     this.setState({
-      customers: '',
-      completed: 0
+      customers: [],
+      completed: 0,
+      searchKeyword: ''
     });
     this.callApi()
     .then(res => this.setState({customers: res}))
@@ -137,7 +141,39 @@ class App extends Component{
     this.setState({ completed: completed > 100 ? 0 : completed + 1});
   }
 
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+
   render(){
+    const filteredComponents = (data) => {
+      data = data.filter((c) => {
+        return c.NAME.indexOf(this.state.searchKeyword) > -1 ;
+      });
+      return data.map((c) => ( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} NAME={c.NAME} birthday={c.birthday} gender={c.gender} job={c.job}/>
+    ));
+    }
+
+    /* 인규야 고마워
+    const filteredComponents = (data) => {
+      return data.filter((c) => c.NAME.indexOf(this.state.searchKeyword) > -1)
+                  .map((c) => (
+                  <Customer
+                    stateRefresh={this.stateRefresh}
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    NAME={c.NAME}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                ));
+    }
+    */
+    
     const {classes} = this.props;
     const cellList = ["번호", "프로필 이미지", "이름", "생년월일", "성별", "직업", "설정"];
     return (
@@ -161,6 +197,9 @@ class App extends Component{
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
+              name="searchKeyword"
+              value={this.state.searchKeyword}
+              onChange={this.handleValueChange}
             />
           </div>
         </Toolbar>
@@ -178,15 +217,15 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-            { this.state.customers ? this.state.customers.map(c => { 
-                return (<Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} NAME={c.NAME} birthday={c.birthday} gender={c.gender} job={c.job} />) 
-              }) : 
-              <TableRow>
-                <TableCell colSpan="6" align="center">
-                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-                </TableCell>
-              </TableRow>
-              }
+            { this.state.customers ?
+              filteredComponents(this.state.customers)
+            :
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
